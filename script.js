da5ec642caed04ff7250b916a743fbb6c8c192e0
JackBrasesco <jacbras@nuevaschool.org>
2016-10-21 08:43:34 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ onNewListItem("product-list", function(product) {
   console.log(product)
   var nameHeader = $("<h1 class='product-name'>" + product.name + "</h1>")
   var image = $("<img class='product-image'>").attr("src", product.picture)
-  var price = $("
+  var price = $("<div id='price'>").html(product.price)
 
   $(".product-list-container").append(nameHeader, image)
 })
